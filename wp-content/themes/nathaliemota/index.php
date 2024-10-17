1f40517eb3a46b7395a6ea2c6f706b6232319c6d
pierrebultez <pierre.bultez@proton.me>
2024-10-17 08:01:38 +0200

    hero first version

--- wp-content/themes/nathaliemota/index.php
+++ wp-content/themes/nathaliemota/index.php
@@ -1,6 +1,11 @@
 <div class="main">
 <?php get_header(); ?>
 
+    <div class="hero">
+        <img src="<?php echo get_stylesheet_directory_uri() . '/assets/images/nathalie-6.webp'; ?>" alt="Photo d'une soirée dansante montrant un couple enlacé">
+        <h1 class="hero-title">Photographe event</h1>
+    </div>
+
 <h1>Hello world !</h1>
 <h2>Hello world !</h2>
 <h3>Hello world !</h3>
