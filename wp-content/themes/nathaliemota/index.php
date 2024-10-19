11995bd8a242a45d88c17afb0a81dc3fb529a05e
pierrebultez <pierre.bultez@proton.me>
2024-10-19 09:45:44 +0200

    added : front-page.php

--- wp-content/themes/nathaliemota/index.php
+++ wp-content/themes/nathaliemota/index.php
@@ -1,10 +1,2 @@
-<?php get_header(); ?>
-    <div class="main">
-
-    <div class="hero">
-        <img src="<?php echo get_stylesheet_directory_uri() . '/assets/images/nathalie-6.webp'; ?>" alt="Photo d'une soirée dansante montrant un couple enlacé">
-        <h1 class="hero-title">Photographe event</h1>
-    </div>
-
-    </div>
-<?php get_footer(); ?>
+<?php
+"Silence is golden";
