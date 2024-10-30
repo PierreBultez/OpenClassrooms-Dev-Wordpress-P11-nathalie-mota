564ceacfe8ddd12bcf3c38aed882d3acdfb1e1b5
pierrebultez <pierre.bultez@proton.me>
2024-10-30 18:24:34 +0100

    OPTIMIZE scripts.js optimize code + commentary

--- wp-content/themes/nathaliemota/scripts/scripts.js
+++ wp-content/themes/nathaliemota/scripts/scripts.js
@@ -1,33 +1,30 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', () => {
     const burger = document.querySelector('.burger-menu');
     const menu = document.querySelector('.menu');
     const icon = burger.querySelector('i');
     const modal = document.getElementById('contactModal');
     const contactLink = document.querySelector('a[href="#contact"]');
 
-    burger.addEventListener('click', function () {
-        menu.classList.toggle('active');
-        if (menu.classList.contains('active')) {
-            icon.classList.remove('fa-bars');
-            icon.classList.add('fa-xmark');
-        } else {
-            icon.classList.remove('fa-xmark');
-            icon.classList.add('fa-bars');
-        }
-        const isExpanded = burger.getAttribute('aria-expanded') === 'true' || false;
-        burger.setAttribute('aria-expanded', !isExpanded);
+    // Gère le clic sur le bouton burger pour afficher/masquer le menu
+    burger.addEventListener('click', () => {
+        menu.classList.toggle('active'); // Affiche ou cache le menu
+        icon.classList.toggle('fa-bars'); // Change l'icône en barre
+        icon.classList.toggle('fa-xmark'); // Change l'icône en croix
+        burger.setAttribute('aria-expanded', menu.classList.contains('active')); // Met à jour l'attribut aria-expanded
     });
 
+    // Vérifie si le lien de contact et la modal existent
     if (contactLink && modal) {
-        // Ajouter l'écouteur d'événement seulement si le lien et la modale existent
-        contactLink.addEventListener('click', function (e) {
-            e.preventDefault(); // Empêche le comportement par défaut du lien
-            modal.classList.add('active'); // Ajoute la classe active pour afficher la modale
+        // Gère le clic sur le lien de contact pour ouvrir la modal
+        contactLink.addEventListener('click', (e) => {
+            e.preventDefault();
+            modal.classList.add('active'); // Affiche la modal
         });
 
-        modal.addEventListener('click', function (e) {
-            if (e.target === modal) { // Si le clic est sur l'overlay (en dehors de la boîte)
-                modal.classList.remove('active'); // Fermer la modale
+        // Gère le clic en dehors de la modal pour la fermer
+        modal.addEventListener('click', (e) => {
+            if (e.target === modal) {
+                modal.classList.remove('active'); // Cache la modal
             }
         });
     }
